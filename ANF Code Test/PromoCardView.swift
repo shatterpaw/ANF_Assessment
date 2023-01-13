6a0926c3f968e9b9db3a6a82a0e49eb4ed288974
Chris Jarvi <mybaldhead@hotmail.com>
2023-01-13 12:41:41 -0800

    extract promo card content into state variables

--- ANF Code Test/PromoCardView.swift
+++ ANF Code Test/PromoCardView.swift
@@ -8,41 +8,51 @@
 import SwiftUI
 
 struct PromoCardView: View {
+    @State var imageName: String = "anf-US-20160518-app-men-tees"
+    @State var topDescription: String = "A & F Essentials"
+    @State var title: String = "Tops Starting at $12"
+    @State var promoMessage: String = "Use Code: 12345"
+    @State var bottomDescription: String = "*In stores & online. "
+    @State var bottomDescriptionUnderlined: String = "Exclusions apply. See Details"
+    @State var content: [ContentItem] = [
+        ContentItem(elementType: nil, target: "", title: "Shop Men"),
+        ContentItem(elementType: nil, target: "", title: "Shop Women")
+    ]
+    
     var body: some View {
         VStack(spacing: 10) {
-            Image("anf-US-20160518-app-men-tees")
+            Image(imageName)
                 .resizable()
                 .scaledToFit()
-            Text("A & F Essentials")
+            
+            Text(topDescription)
                 .textCase(.uppercase)
-            Text("Tops Starting at $12")
+            
+            Text(title)
                 .font(.system(size: 17, weight: .bold))
                 .textCase(.uppercase)
-            Text("Use Code: 12345")
+            
+            Text(promoMessage)
                 .font(.system(size: 11))
+            
             HStack(spacing: 0) {
-                Text("* In stores & online. ")
-                Text("Exclusions apply. See Details")
+                Text(bottomDescription)
+                Text(bottomDescriptionUnderlined)
                     .underline()
             }
-            VStack {
-                Button(action: {}) {
-                    Text("Shop Men")
+            
+            ForEach(content, id: \.self) { item in
+                Button {
+                    
+                } label: {
+                    Text(item.title)
                         .padding()
                         .frame(maxWidth: .infinity)
-                }
-                .buttonStyle(.plain)
-                .border(.gray)
-                Button(action: {}) {
-                    Text("Shop Women")
-                        .padding()
-                        .frame(maxWidth: .infinity)
+                        .font(.system(size: 15))
                 }
                 .buttonStyle(.plain)
                 .border(.gray)
             }
-            .font(.system(size: 15))
-            .padding([.leading, .trailing])
         }
         .font(.system(size: 13))
         .padding()
