0427937feb87aa1e28a058f6ffcd17f517c98521
Chris Jarvi <mybaldhead@hotmail.com>
2023-01-13 13:24:54 -0800

    decode json data to PromoItem object

--- ANF Code Test/ANFExploreCardTableViewController.swift
+++ ANF Code Test/ANFExploreCardTableViewController.swift
@@ -8,11 +8,19 @@ import SwiftUI
 
 class ANFExploreCardTableViewController: UITableViewController {
 
-    private var exploreData: [[AnyHashable: Any]]? {
+//    private var exploreData: [[AnyHashable: Any]]? {
+//        if let filePath = Bundle.main.path(forResource: "exploreData", ofType: "json"),
+//         let fileContent = try? Data(contentsOf: URL(fileURLWithPath: filePath)),
+//         let jsonDictionary = try? JSONSerialization.jsonObject(with: fileContent, options: .mutableContainers) as? [[AnyHashable: Any]] {
+//            return jsonDictionary
+//        }
+//        return nil
+//    }
+    private var exploreData: [PromoItem]? {
         if let filePath = Bundle.main.path(forResource: "exploreData", ofType: "json"),
-         let fileContent = try? Data(contentsOf: URL(fileURLWithPath: filePath)),
-         let jsonDictionary = try? JSONSerialization.jsonObject(with: fileContent, options: .mutableContainers) as? [[AnyHashable: Any]] {
-            return jsonDictionary
+           let fileContent = try? Data(contentsOf: URL(fileURLWithPath: filePath)),
+           let items = try? JSONDecoder().decode([PromoItem].self, from: fileContent) {
+            return items
         }
         return nil
     }
@@ -23,13 +31,17 @@ class ANFExploreCardTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = self.tableView.dequeueReusableCell(withIdentifier: "ExploreContentCell", for: indexPath)
-        if let titleLabel = cell.viewWithTag(1) as? UILabel,
-           let titleText = exploreData?[indexPath.row]["title"] as? String {
-            titleLabel.text = titleText
+
+        guard let item = exploreData?[indexPath.row] else {
+            return cell
+        }
+        
+        if let titleLabel = cell.viewWithTag(1) as? UILabel {
+            titleLabel.text = item.title
         }
         
         if let imageView = cell.viewWithTag(2) as? UIImageView,
-           let name = exploreData?[indexPath.row]["backgroundImage"] as? String,
+           let name = item.backgroundImageName,
            let image = UIImage(named: name) {
             imageView.image = image
         }
